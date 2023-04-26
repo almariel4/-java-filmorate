769b0cab908c49d6b23541abef549b5224f4dc35
Наумов Максим <dusa11223344@yandex.ru>
2023-04-27 01:37:45 +0300

    CheckStyle git

--- src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -14,7 +14,6 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping( "/directors")
 public class DirectorController {
-
     private final DirectorService directorService;
 
     @PostMapping
